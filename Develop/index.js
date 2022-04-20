6b2fd59102b2ee9383c43eca52f038762d3b3c56
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-04-20 17:55:08 -0400

    added questions that will be asked to the question array

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,43 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    {type: 'input',
+    message: 'What is your user name?',
+    name: 'name',
+  },
+  {type: 'input',
+    message: 'What is your gitHub username?',
+    name: 'githubusername',
+  },
+  {type: 'list',
+    message: 'What license would you list to use?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    name: 'license',
+  },
+  {type: 'input',
+  message: 'What is your email',
+  name: 'email',
+},
+{type: 'input',
+message: 'What is the link to your github profile?',
+name: 'githublink',
+},
+{type: 'input',
+message: 'Please type a description of your project',
+name: 'description',
+},
+{type: 'input',
+message: 'Expalin how to install your applicaton.',
+name: 'installation',
+},
+{type: 'input',
+message: 'Explain how your application is used.',
+name: 'usage',
+},
+{type: 'input',
+message: 'Explaing what tests you would run here',
+name: 'tests',
+},
 
 ];
 
@@ -16,6 +53,7 @@ function init() {}
 
 // Function call to initialize app
 init();
+
 
 
 
