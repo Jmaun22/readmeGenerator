800d84a5a5baaeb14eb94f4f7f44c25b6de36e42
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-04-20 17:43:14 -0400

    added npm package inquier for this project

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,12 @@
 // TODO: Include packages needed for this application
 
+var inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +16,8 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+
